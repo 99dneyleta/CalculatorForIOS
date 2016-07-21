2e1da639e60f6073b113db0746b72e53e3297dea
99dneyleta <programmerversusgod@gmail.com>
2016-07-21 23:25:28 +0300

    First app for ios

---
+++ calculatorForIOSDevices/calculator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by MacBook on 7/7/16.
+//  Copyright © 2016 MacBook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        UIApplication.sharedApplication().statusBarStyle = .LightContent
+        
+     //lklkm
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
+
